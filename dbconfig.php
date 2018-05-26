d83f16ad1c31776de140bf17173bcfd244662b57
iphpi <q_dl@sina.com>
2018-05-26 20:40:18 +0800

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,6 +1,6 @@
 <?php
 //数据库配置文件
-define("HOST","localhost");//主机
+define("HOST","120.79.255.111");//主机
 define("USER","root");//用户名
-define("PASS","root");//密码
+define("PASS","myroot");//密码
 define("DBNAME","demodb");//数据库名
